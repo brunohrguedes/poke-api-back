211e7382cd4939eef222aff6a0fff213c8a6a2f9
Bruno Helder <brunohrguedes@gmail.com>
2024-06-25 17:56:55 -0300

    Create GET Pokémon data by name

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
 
+const pokeApiUrl = process.env.POKE_API_URL;
 const app = express();
 const port = 3000;
 
@@ -7,6 +10,25 @@ app.get("/", (req, res) => {
   res.send(
     "A Pokémon API created to fetch Pokémon habilites and return them in alphabetical order"
   );
+});
+
+app.get("/api/pokemons/:name", async (req, res) => {
+  const normalizedName = req.params.name.toLowerCase();
+
+  const pokeApiResponse = await axios
+    .get(`${pokeApiUrl}/pokemon/${normalizedName}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.error(error));
+
+  const data = {
+    name: pokeApiResponse.name,
+    abilities: pokeApiResponse.abilities,
+    frontSprite: pokeApiResponse.sprites.front_default,
+  };
+
+  res.send(data);
 });
 
 app.listen(port, () => {
